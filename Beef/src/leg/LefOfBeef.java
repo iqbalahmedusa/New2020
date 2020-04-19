3964cc4f6663d9857ae12e36b419561876062902
iqbal <iqbal@DESKTOP-CKDMN9M.home>
2020-04-18 22:33:42 -0400

    Hi My project

---
+++ Beef/src/leg/LefOfBeef.java
@@ -0,0 +1,17 @@
+package leg;
+
+public class LefOfBeef {
+	static void myAdd() {
+		int x =66;
+		int y=88;
+		int z = x+y;
+		System.out.println(z);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("four");
+		myAdd();
+
+	}
+
+}
